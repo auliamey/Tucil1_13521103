ecff731f4d803a401aeec7d03be337fb9dd837d8
auliamey <auliaannandya@gmail.com>
2023-01-21 18:51:25 +0700

    savefile and fix debug

--- game.cpp
+++ game.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <time.h>
 using namespace std;
@@ -143,47 +144,92 @@ void is24(string a, string b, string c, string d, vector<string>& arrayhasil, in
 
 int main() {
     string card1, card2, card3, card4;
+    string input;
+    string w1, w2, w3, w4;
+    vector<string> arrpermute, arrayofhasil;
 
-    cout << "Masukkan kartu pertama: ";
-    cin >> card1;
-    bool cek = checkInput(card1);
-    while (cek == false){
+
+    ofstream output("/Users/auliameydivaannandya/Documents/GitHub/24-Games/output.txt", std::ofstream::out);
+
+    cout << "Pilihan Input : \n";
+    cout << "1. Random Number \n2. Input User\n";
+    cout << "Masukkan pilihan : ";
+    cin >> input;
+
+    while ((input != "1") and (input != "2")){
         cout << "\n*    INPUT SALAH!!!    * \n* Masukkan Input Ulang *\n\n";
+        cout << "Masukkan pilihan : ";
+        cin >> input;
+    }
+
+    if (input == "1"){
+        srand(time(NULL));
+
+        string arrofcards[14] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+        int randIndex1 = rand() % 13;
+        int randIndex2 = rand() % 13;
+        int randIndex3 = rand() % 13;
+        int randIndex4 = rand() % 13;
+        card1 = arrofcards[randIndex1];
+        card2 = arrofcards[randIndex2];
+        card3 = arrofcards[randIndex3];
+        card4 = arrofcards[randIndex4];
+
+        cout << "Kartu yang didapatkan : " << endl << card1 << " " << card2 << " " << card3 << " " << card4 << endl;
+    } else if (input == "2"){
         cout << "Masukkan kartu pertama: ";
         cin >> card1;
-        cek = checkInput(card1);
-    }
-    cout << "Masukkan kartu kedua: ";
-    cin >> card2;
-    cek = checkInput(card2);
-    while (cek == false){
-        cout << "\n*    INPUT SALAH!!!    * \n* Masukkan Input Ulang *\n\n";
+        bool cek = checkInput(card1);
+        while (cek == false){
+            cout << "\n*    INPUT SALAH!!!    * \n* Masukkan Input Ulang *\n\n";
+            cout << "Masukkan kartu pertama: ";
+            cin >> card1;
+            cek = checkInput(card1);
+        }
         cout << "Masukkan kartu kedua: ";
         cin >> card2;
         cek = checkInput(card2);
-    }
-    cout << "Masukkan kartu ketiga: ";
-    cin >> card3;
-    cek = checkInput(card3);
-    while (cek == false){
-        cout << "\n*    INPUT SALAH!!!    * \n* Masukkan Input Ulang *\n\n";
+        while (cek == false){
+            cout << "\n*    INPUT SALAH!!!    * \n* Masukkan Input Ulang *\n\n";
+            cout << "Masukkan kartu kedua: ";
+            cin >> card2;
+            cek = checkInput(card2);
+        }
         cout << "Masukkan kartu ketiga: ";
         cin >> card3;
         cek = checkInput(card3);
-    }
-    cout << "Masukkan kartu keempat: ";
-    cin >> card4;
-    cek = checkInput(card4);
-    while (cek == false){
-        cout << "\n*    INPUT SALAH!!!    * \n* Masukkan Input Ulang *\n\n";
+        while (cek == false){
+            cout << "\n*    INPUT SALAH!!!    * \n* Masukkan Input Ulang *\n\n";
+            cout << "Masukkan kartu ketiga: ";
+            cin >> card3;
+            cek = checkInput(card3);
+        }
         cout << "Masukkan kartu keempat: ";
         cin >> card4;
         cek = checkInput(card4);
+        while (cek == false){
+            cout << "\n*    INPUT SALAH!!!    * \n* Masukkan Input Ulang *\n\n";
+            cout << "Masukkan kartu keempat: ";
+            cin >> card4;
+            cek = checkInput(card4);
+        }
+    }
+
+    if (card1 == "10"){
+        card1 = "Z";
+    }
+    if (card2 == "10"){
+        card2 = "Z";
+    }
+    if (card3 == "10"){
+        card3 = "Z";
+    }
+    if (card4 == "10"){
+        card4 = "Z";
     }
 
     string fullcard = card1 + card2 + card3 + card4;
-    string w1, w2, w3, w4;
-    vector<string> arrpermute, arrayofhasil;
     permutation(fullcard, 0, arrpermute);
     int ctr = 0;
 
@@ -192,24 +238,75 @@ int main() {
 
         for (int j=0; temp[j]; ++j){
             if (j == 0){
-                w1 = temp[j];
+                if (temp[j] == 'Z'){
+                    w1 = "10";
+                } else {
+                    w1 = temp[j];
+                }
             } else if (j == 1){
-                w2 = temp[j];
+                if (temp[j] == 'Z'){
+                    w2 = "10";
+                } else {
+                    w2 = temp[j];
+                }
             } else if (j == 2){
-                w3 = temp[j];
+                if (temp[j] == 'Z'){
+                    w3 = "10";
+                } else {
+                    w3 = temp[j];
+                }
             } else if (j == 3) {
-                w4 = temp[j];
+                if (temp[j] == 'Z'){
+                    w4 = "10";
+                } else {
+                    w4 = temp[j];
+                }
             }
         }
 
         is24(w1, w2, w3, w4, arrayofhasil, ctr);
     }
 
-    cout << "Terdapat " << ctr << " solusi yang ditemukan.\n";
+    string solve, savefile;
 
-    for (int i=0; i<arrayofhasil.size(); i++){
-        cout << arrayofhasil.at(i) << "\n";
+    cout << "\nTerdapat " << ctr << " solusi yang ditemukan.\n";
+
+    if (ctr != 0){
+        cout << "Show the solve? (yes/no) : ";
+        cin >> solve;
+
+        while ((solve != "yes") and (solve != "no")){
+            cout << "Input yang anda masukkan salah!\n Masukkan input ulang!\n";
+            cout << "Tunjukkan hasil? (yes/no) : ";
+            cin >> solve;
+        }
+
+        if (solve == "yes"){
+            cout << "Menunjukkan hasil :\n";
+            for (int i=0; i<arrayofhasil.size(); i++){
+                cout << arrayofhasil.at(i) << "\n";
+            }
+        }
+
+        cout << "\nApakah Anda ingin menyimpan di file? (yes/no) :  ";
+        cin >> savefile;
+        while ((savefile != "yes") and (savefile != "no")){
+            cout << "Input yang anda masukkan salah!\n Masukkan input ulang!\n";
+            cout << "\nApakah Anda ingin menyimpan di file? (yes/no) :  ";
+            cin >> savefile;
+        }
+
+        if (savefile == "yes"){
+            if (output.is_open()){
+                for (int i=0; i<arrayofhasil.size(); i++){
+                    output << arrayofhasil.at(i) << "\n";
+                }
+                output.close();
+            } else {
+                cout << "Terdapat masalah dalam pembukaan file\n";
+            }
+        }
     }
-
+    
     return 0;
 }
